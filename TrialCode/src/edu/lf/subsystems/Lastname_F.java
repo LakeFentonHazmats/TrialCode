b08ef40b72c6843a1abe4bab313e012f3bcb6d12
jwebster <jwebster@HS_B213_D03.lfcs.local>
2014-07-09 16:26:24 -0400

    Brief summary of the code changes, no text language

---
+++ TrialCode/src/edu/lf/subsystems/Lastname_F.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.lf.subsystems;
+
+/**
+ *Please change these comments to include your favorite soda pop.
+ * @author jwebster
+ */
+public class Lastname_F {
+    
+}
